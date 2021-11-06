1752f95c672b9b3ebeea856af10f694e4e6960ad
Kio-dev-it <pas.petito@gmail.com>
2021-11-06 14:26:52 +0100

    Add mega-link section in main

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,4 +29,49 @@
         <button class="btn-load">Load More</button>
 
     </div>
+
+    <div class="mega-link">
+        <div class="container">
+            <a href="#" class="mega-link__card">
+                <div class="mega-link__image">
+                    <img class="mega-link__image" src="{{asset("images/buy-comics-digital-comics.png")}}" alt="">
+                </div>
+                <div class="mega-link__title">
+                    <p>Digital Comics</p>
+                </div>
+            </a>
+            <a href="#" class="mega-link__card">
+                <div class="mega-link__image">
+                    <img class="mega-link__image" src="{{asset("images/buy-comics-merchandise.png")}}" alt="">
+                </div>
+                <div class="mega-link__title">
+                    <p>Dc merchandise</p>
+                </div>
+            </a>
+            <a href="#" class="mega-link__card">
+                <div class="mega-link__image">
+                    <img class="mega-link__image" src="{{asset("images/buy-comics-subscriptions.png")}}" alt="">
+                </div>
+                <div class="mega-link__title">
+                    <p>subscription</p>
+                </div>
+            </a>
+            <a href="#" class="mega-link__card">
+                <div class="mega-link__image">
+                    <img class="mega-link__image" src="{{asset("images/buy-comics-shop-locator.png")}}" alt="">
+                </div>
+                <div class="mega-link__title">
+                    <p>Comic Shop Locator</p>
+                </div>
+            </a>
+            <a href="#" class="mega-link__card">
+                <div class="mega-link__image">
+                    <img class="mega-link__image" src="{{asset("images/buy-dc-power-visa.svg")}}" alt="">
+                </div>
+                <div class="mega-link__title">
+                    <p>Dc power visa</p>
+                </div>
+            </a>
+        </div>
+    </div>
 @endsection
