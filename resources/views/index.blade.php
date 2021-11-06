8f6d249ccb3f370f18a471e60a5df4027bc3d700
Kio-dev-it <pas.petito@gmail.com>
2021-11-06 12:44:15 +0100

    Add main

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,32 @@
 @extends('layout.base')
 
 @section('pageContent')
-    <h1>Hello LaraComics!</h1>
+    <div class="hero">
+        <img class="hero__image" src="{{asset("images/jumbotron.jpg")}}" alt="">
+    </div>
 
-    <ul>
-        @foreach ($comics as $comic)
-            <li>{{$comic["title"]}}</li>
-        @endforeach
-    </ul>
+    <div class="container">
+
+        <div class="section-tag">
+            <h2>current series</h2>
+        </div>
+
+        <div class="card-wrapper">
+
+            @foreach ($comics as $comic)
+                <div class="card">
+                    <div class="card__image">
+                        <img src="{{$comic["thumb"]}}" alt="{{$comic["title"]}} Cover">
+                    </div>
+                    <div class="card__title">
+                        <p>{{$comic["series"]}}</p>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+
+        <button class="btn-load">Load More</button>
+
+    </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,23 +11,12 @@
             <ul class="navigation">
 
                 @foreach ($navmenu as $linkmenu)
-                <li><a href="{{$linkmenu["url"]}}" class="{{$linkmenu["state"]}}">{{$linkmenu["name"]}}</a></li>
-                    
+                    <li><a href="{{$linkmenu["url"]}}" class="{{$linkmenu["state"]}}">{{$linkmenu["name"]}}</a></li>
                 @endforeach
 
-                {{-- <li><a href="#">link 1</a></li>
-                <li><a href="#" class="active">link 2</a></li>
-                <li><a href="#">link 3</a></li>
-                <li><a href="#">link 4</a></li>
-                <li><a href="#">link 5</a></li>
-                <li><a href="#">link 6</a></li>
-                <li><a href="#">link 7</a></li>
-                <li><a href="#">link 8</a></li>
-                <li><a href="#">link 9</a></li>
-                <li><a href="#">link 10</a></li> --}}
             </ul>
             <div class="search">
-                <input type="text" name="search" id="" placeholder="Search">
+                <input type="text" name="search" placeholder="Search">
                 <button><i class="fas fa-search"></i></button>
             </div>
         </div>
