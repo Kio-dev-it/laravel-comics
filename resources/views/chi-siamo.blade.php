c1886140931b3e7ad6e4b723219918128400a2fc
Kio-dev-it <pas.petito@gmail.com>
2021-11-08 07:50:00 +0100

    fix use of "main" in pageContent

--- resources/views/chi-siamo.blade.php
+++ resources/views/chi-siamo.blade.php
@@ -2,10 +2,10 @@
 
 @section('pageContent')
 
-    <main class="main main__chi-siamo">
+    <section class="section__chi-siamo">
         <div class="container chi-siamo">
             <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quidem tempora earum voluptas, corporis minus ipsa minima libero facilis sed neque. Officia, dolorum dolores. Dolor, quidem? Consequatur quia minima error.</h2>
         </div>
-    </main>
+    </section>
     
 @endsection
